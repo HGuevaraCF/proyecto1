45034247f4283be3124cc165e4d2b8f4a64bb5f3
HGuevaraCF <87338085+HGuevaraCF@users.noreply.github.com>
2022-04-22 00:11:37 -0500

    add manual expense modal

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,14 +1,26 @@
 let collapsed = true;
 
 //-------------sidebar behaviour--------------//
-function collapsibleSidebar(){
-    if(collapsed){
-        document.getElementById("sidebar").style.width = "250px";
-        document.getElementById("main").style.marginLeft = "250px";
-        collapsed = false;
-    }else if(!collapsed){
-        document.getElementById("sidebar").style.width = "0px";
-        document.getElementById("main").style.marginLeft = "0px";
-        collapsed = true;
+    function collapsibleSidebar(){
+        if(collapsed){
+            document.getElementById("sidebar").style.width = "250px";
+            document.getElementById("main").style.marginLeft = "250px";
+            collapsed = false;
+        }else if(!collapsed){
+            document.getElementById("sidebar").style.width = "0px";
+            document.getElementById("main").style.marginLeft = "0px";
+            collapsed = true;
+        }
     }
-}
+//-----------Modal show----------------------//
+    function toggleModal(modalID){
+        document.getElementById(modalID).classList.toggle("hidden");
+        document.getElementById(modalID + "-backdrop").classList.toggle("hidden");
+        document.getElementById(modalID).classList.toggle("flex");
+        document.getElementById(modalID + "-backdrop").classList.toggle("flex");
+    }
+
+//---------Add Expense from modal------------//
+    function AddExpense(date, name, amount) {
+        
+    }
